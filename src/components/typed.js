e3e14d7006eb5018c2c416f1bc807fb8324f6242
Luichix <luigui.rex@gmail.com>
2022-03-24 20:10:30 -0600

    actualizacion de portafolio

---
+++ src/components/typed.js
@@ -0,0 +1,28 @@
+import React, { useRef, useEffect } from 'react'
+import Typed from 'typed.js'
+
+const Instruction = () => {
+  const typeTarget = useRef(null)
+
+  useEffect(() => {
+    const typed = new Typed(typeTarget.current, {
+      strings: [
+        'Programación en Microsoft Excel',
+        'Sistemas de Información',
+        'Administración de Empresas'
+      ],
+      typeSpeed: 50,
+      backSpeed: 40,
+      backDelay: 800,
+      loop: true,
+      loopCount: Infinity
+    })
+
+    return () => {
+      typed.destroy()
+    }
+  }, [])
+
+  return <span className="text-light" ref={typeTarget} />
+}
+export default Instruction
--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.js'
-import './scss/styles.scss'
+import './css/styles.css'
 
 ReactDOM.render(
+  <React.StrictMode>
     <BrowserRouter>
         <App/>
-    </BrowserRouter>,
-    window.document.getElementById('app')
-)
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
