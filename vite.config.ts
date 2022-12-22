f758b5bd1409b4a889366ae92009ee72b8b4e953
Luichix <luichix.rex@gmail.com>
2022-12-22 00:39:11 -0600

    adjust base vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/portfolio/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
